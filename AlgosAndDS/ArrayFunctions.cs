9ac42ab93d3aa012495804e25e6064ea688b0d39
Cinnam00n <cinam0n@proton.me>
2024-11-28 13:51:41 +0000

    Add project files.

---
+++ AlgosAndDS/ArrayFunctions.cs
@@ -0,0 +1,13 @@
+﻿namespace Functions;
+public static class ArrayFunctions
+{
+    public static int[] GetOrderedArray(int numberOfElements)
+    {
+        var array = new int[numberOfElements];
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            array[i] = i;
+        }
+        return array;
+    }
+}
---
+++ AlgosAndDS/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Functions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var array = ArrayFunctions.GetOrderedArray(5);
+
+        Console.WriteLine("[{0}]", string.Join(", ", array));
+
+        Console.ReadLine();
+    }
+}
